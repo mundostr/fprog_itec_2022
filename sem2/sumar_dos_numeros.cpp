f5aab7cd03d2765f483cdfcac41ddc75434c5f83
mundostr <idux.net@gmail.com>
2022-08-16 17:50:49 -0300

    first commit

---
+++ sem2/sumar_dos_numeros.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main() {
+	int a;
+	int b;
+	int sum;
+	
+	cout << "Ingresar a: ";
+	cin >> a;
+	cout << "Ingresar b: ";
+	cin >> b;
+
+	sum = a + b;
+
+	cout << "Suma: " << sum;
+	
+	return 0;
+}
