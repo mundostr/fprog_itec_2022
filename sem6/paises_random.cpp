d42c7e8f15782ef75b40585bf3249967d8ab2fc1
mundostr <idux.net@gmail.com>
2022-09-05 19:20:20 -0300

    Ej paises_random

--- sem6/paises_random.cpp
+++ sem6/paises_random.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
-#include <string>
-#include <stdio.h>
+#include <string> // Manejo de pseudoclase string
+#include <stdio.h> // printf
+#include <vector>
+#include <Windows.h>
+#include <ctime>
+#include <algorithm>
 
 using namespace std;
 
 const int CTD_PAISES = 10;
+const int CTD_PAISES_SEL = 5;
 
 int main() {
+    srand(time(NULL));
+    SetConsoleOutputCP(CP_UTF8);
+    
     // Matriz estática
     string mtr_paises[CTD_PAISES] = {"Argentina", "Brasil", "Uruguay", "Paraguay", "Chile", "Bolivia", "Ecuador", "Colombia", "Peru", "Venezuela"};
+    vector<string> mtr_paises_sel = {};
 
-    for (int c1 = 0; c1 < CTD_PAISES; c1++) {
-        printf("%s\n", mtr_paises[c1].c_str());
+    // mtr_paises_sel.clear();
+    // for (int c1 = 0; c1 < CTD_PAISES_SEL; c1++) {
+    while(mtr_paises_sel.size() < CTD_PAISES_SEL) {
+        string pais_al_azar = mtr_paises[rand() % CTD_PAISES];
+
+        if (count(mtr_paises_sel.begin(), mtr_paises_sel.end(), pais_al_azar) == 0) {
+            mtr_paises_sel.push_back(pais_al_azar);
+        }
+    }
+
+    for (int c1 = 0; c1 < CTD_PAISES_SEL; c1++) {
+        printf("%s\n", mtr_paises_sel[c1].c_str());
     }
 
     return 0;
