4c0c865973ee0b9550e3cdf1c732029993dcbbc9
mundostr <idux.net@gmail.com>
2022-09-12 19:21:39 -0300

    Orden matrices con sort

--- sem7/orden_matrices.cpp
+++ sem7/orden_matrices.cpp
@@ -3,34 +3,37 @@ Práctica de ordenamiento manual y automático de matriz
 */
 
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main() {
-    int mtr_lecturas[] = {22, 23, 18, 15, 21};
+    int mtr_lecturas[] = {22, 23, 18, 15, 18, 21};
     
     int indice = 0;
     int indice_menor = 0;
     bool intercambiar = false;
 
-    while(indice < size(mtr_lecturas)) {
-        int menor = mtr_lecturas[indice];
+    sort(begin(mtr_lecturas), end(mtr_lecturas));
+
+    // while(indice < size(mtr_lecturas)) {
+    //     int menor = mtr_lecturas[indice];
         
-        for (int x = indice; x < size(mtr_lecturas); x++) {
-            if (mtr_lecturas[x] < menor) {
-                menor = mtr_lecturas[x];
-                indice_menor = x;
-                intercambiar = true;
-            }
-        }
+    //     for (int x = indice; x < size(mtr_lecturas); x++) {
+    //         if (mtr_lecturas[x] < menor) {
+    //             menor = mtr_lecturas[x];
+    //             indice_menor = x;
+    //             intercambiar = true;
+    //         }
+    //     }
 
-        if (intercambiar == true) {
-            swap(mtr_lecturas[indice], mtr_lecturas[indice_menor]);
-            intercambiar = false;
-        }
+    //     if (intercambiar == true) {
+    //         swap(mtr_lecturas[indice], mtr_lecturas[indice_menor]);
+    //         intercambiar = false;
+    //     }
 
-        indice++;
-    }
+    //     indice++;
+    // }
 
     // Muestro como quedó ordenada la matriz
     for (int x = 0; x < size(mtr_lecturas); x++) {
