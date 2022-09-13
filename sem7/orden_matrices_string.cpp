670b98564d9e234bb6cfed91ae4d04a6e6f436aa
mundostr <idux.net@gmail.com>
2022-09-13 14:31:28 -0300

    Ordenamiento strings con vectores

---
+++ sem7/orden_matrices_string.cpp
@@ -0,0 +1,26 @@
+/*
+Práctica de ordenamiento de strings con sort(), utilizando vectores
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    vector<string> mtr_nombres = {"Pepe", "Pepa", "Carlos", "Carolina", "Pepo"};
+    
+    int indice = 0;
+    int indice_menor = 0;
+    bool intercambiar = false;
+
+    sort(mtr_nombres.begin(), mtr_nombres.end());
+
+    for (string nombre: mtr_nombres) {
+        printf("%s, ", nombre.c_str());
+    }
+    printf("\n");
+
+    return 0;
+}
